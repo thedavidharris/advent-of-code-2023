037eab14729944a41aec2486984d003597ae02da
David Harris <davidaharris@outlook.com>
2023-12-01 19:13:05 -0500

    chore: code golf

--- day1/1.py
+++ day1/1.py
@@ -1,26 +1,15 @@
 from aocd import get_data
 import re
 
-data = get_data(day=1, year=2023)
+data = get_data(day=1, year=2023).strip()
 
 # Part 1
 def calibration(input):
-    return sum(int(digits[0] + digits[-1]) for line in input if (digits := re.findall(r'\d', line)))
+    return sum(int(x[0]+x[-1]) for x in [re.findall(r"\d", l) for l in input.split("\n")])
 
-print(f'Part 1: {calibration(data.splitlines())}')
+print(f'Part 1: {calibration(data)}')
 
 # Part 2
+for s in "one two three four five six seven eight nine".split(): d=data.replace(s,s+str(len(s))+s)
 
-sanitized_data = (
-    data.replace("one", "one1one")
-        .replace("two", "two2two")
-        .replace("three", "three3three")
-        .replace("four", "four4four")
-        .replace("five", "five5five")
-        .replace("six", "six6six")
-        .replace("seven", "seven7seven")
-        .replace("eight", "eight8eight")
-        .replace("nine", "nine9nine")
-    ).splitlines()
-
-print(f'Part 2: {calibration(sanitized_data)}')
+print(f'Part 2: {calibration(d)}')
